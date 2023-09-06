1375a83247f2cc59b597face0eeead241724c604
davidruc <davidarueda18@gmail.com>
2023-09-06 09:01:00 -0500

    feat: :card_file_box: actualización del script sql para eliminar las configuraciones de variables de entorno

--- src/database/script.sql
+++ src/database/script.sql
@@ -1,36 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 29-04-2023 a las 11:47:13
--- Versión del servidor: 10.4.28-MariaDB
--- Versión de PHP: 8.2.4
+CREATE DATABASE meduino;
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `asistente2`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `cuidador`
---
-
-CREATE asistente2;
-
-USE asistente2;
-
+USE meduino;
 
 CREATE TABLE `cuidador` (
   `id` int(10) UNSIGNED NOT NULL,
@@ -173,8 +143,5 @@ ALTER TABLE `paciente`
 ALTER TABLE `paciente_cuidador`
   ADD CONSTRAINT `paciente_cuidador_ibfk_1` FOREIGN KEY (`paciente_id`) REFERENCES `paciente` (`id`),
   ADD CONSTRAINT `paciente_cuidador_ibfk_2` FOREIGN KEY (`cuidador_id`) REFERENCES `cuidador` (`id`);
-COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
